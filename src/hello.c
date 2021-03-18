c431a37735f4a4944eedca7ced174a1a3ce1b50d
leoking2000 <leoking470@gmail,com>
2021-03-18 11:35:49 +0200

    added utilities with vec2d

--- src/hello.c
+++ src/hello.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "utilities.h"
 
 int main()
 {
     printf("Hello world!!!\n");
+
+    vec2d v1 = { 5.0f, 1.0f };
+    vec2d v2 = { 6.0f, 3.0f}; 
+
+    vec2d k = add(v1, v2);
+    printf("(%.1f , %.1f)\n",k.x,k.y);
+
     return 0;
 }
---
+++ src/utilities.h
@@ -0,0 +1,10 @@
+#pragma once
+
+typedef struct vec2d
+{
+    float x;
+    float y;
+} vec2d;
+
+vec2d add(vec2d a, vec2d b);
+
