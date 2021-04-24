600347627477f54d888db8658b13a5f85695ab38
leoking2000 <leoking470@gmail,com>
2021-04-24 20:05:58 +0300

    24-4-2021

--- src/3190090-3190008-pizza.c
+++ src/3190090-3190008-pizza.c
@@ -1,8 +1,156 @@
 #include "3190090-3190008-pizza.h"
 
+static unsigned int N_cust;
+static unsigned int SEED;
+
+int GetRandomNumber(int min, int max);
+
+ResourceInfo* SetUpResources();
+
+#define TELE 0
+#define COOK 1
+#define OVEN 2
+#define DELIVERY 3
+
+bool Take(ResourceInfo* r, unsigned int amount);
+bool Give(ResourceInfo* r, unsigned int amount);
+
+void* TakeOrder(void* data)
+{
+    Args* arg = (Args*)data;
+
+    int id = arg->id;
+
+    printf("[Thread %i]has started\n", id);
+
+    int pizzas = GetRandomNumber(N_oderlow, N_orderhigh);
+    pthread_mutex_lock(&arg->Resources[OVEN].muxtex);
+
+    while(!Take(&arg->Resources[OVEN], pizzas))
+    {
+        printf("Thread %i] is waiting\n", id);
+        pthread_cond_wait(&arg->Resources[OVEN].cond, &arg->Resources[OVEN].muxtex);
+    }
+
+    pthread_mutex_unlock(&arg->Resources[OVEN].muxtex);
+
+    printf("[Thread %i] %i pizzas are in the oven\n", id, pizzas);
+    sleep(pizzas);
+
+    pthread_mutex_lock(&arg->Resources[OVEN].muxtex);
+
+    Give(&arg->Resources[OVEN], pizzas);
+    pthread_cond_signal(&arg->Resources[OVEN].cond);
+
+    pthread_mutex_unlock(&arg->Resources[OVEN].muxtex);
+
+    printf("[Thread %i]pizzas are ready\n", id);
+    pthread_exit(NULL);
+}
+
 int main(int argc, char** argv)
 {
-    printf(hello);
+    if(argc == 3)
+    {
+        N_cust = atoi(argv[1]);
+        if(N_cust == 0)
+        {
+            printf("[ERROR] Number of customers is not corect! \n");
+            exit(EXIT_FAILURE);
+        }
+
+        SEED = atoi(argv[2]);
+        if(N_cust == 0)
+        {
+            printf("[ERROR] SEED is not corect! \n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else{
+        printf("[ERROR] Number of args must be 2 \n");
+        exit(EXIT_FAILURE);
+    }
+
+    ResourceInfo* Resources = SetUpResources();
+
+    Args* args = (Args*)malloc(N_cust * sizeof(Args));
+
+    ThreadID* arr = (ThreadID*)malloc(N_cust * sizeof(ThreadID));
+
+    for(int i = 0; i < N_cust; i++)
+    {
+        args[i].Resources = Resources;
+        args[i].id = i + 1;
+        
+        int error_code = pthread_create(&arr[i], NULL, TakeOrder, &args[i] );
+
+        if(error_code != 0)
+        {
+            printf("[ERROR] Thread %i failed to be created with code %i \n", i, error_code);
+            exit(EXIT_FAILURE);
+        }
+
+        sleep(1);
+    }
+    
+    for(int i = 0; i < N_cust; i++)
+    {
+        int error_code = pthread_join(arr[i], NULL);
+
+        if(error_code != 0)
+        {
+            printf("[ERROR] Thread %i failed to be Joined with code %i \n", i, error_code);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    free(Resources);
+    free(args);
+    free(arr);
 
     return 0;
+}
+
+int GetRandomNumber(int min, int max)
+{
+    return min + rand_r(&SEED) % ( (max+1) - min );
+}
+
+ResourceInfo* SetUpResources()
+{   
+    ResourceInfo* arr = (ResourceInfo*) malloc(4 * sizeof(ResourceInfo));
+
+    arr[TELE]._MaxSize = N_tel;
+    arr[TELE].size = N_tel;
+
+    arr[COOK]._MaxSize = N_cook;
+    arr[COOK].size = N_cook;
+
+    arr[OVEN]._MaxSize = N_oven;
+    arr[OVEN].size = N_oven;
+
+    arr[DELIVERY]._MaxSize = N_oven;
+    arr[DELIVERY].size = N_oven;
+
+    return arr;
+}
+
+bool Take(ResourceInfo* r, unsigned int amount)
+{
+    if(r->size - amount <= 0)
+    {
+        return FALSE;
+    }
+    r->size = r->size - amount;
+    return TRUE;
+}
+
+bool Give(ResourceInfo* r, unsigned int amount)
+{
+    if(r->size + amount > r->_MaxSize)
+    {
+        return FALSE;
+    }
+    r->size = r->size + amount;
+    return TRUE;
 }
--- src/3190090-3190008-pizza.h
+++ src/3190090-3190008-pizza.h
@@ -1,4 +1,52 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define hello "Hello World\n"
+#define ThreadID pthread_t
+
+#define N_tel 3
+#define N_cook 2
+#define N_oven 10
+#define N_deliverer 7
+
+#define T_OrderLow 1
+#define T_OrderHigh 1
+
+#define N_oderlow 1
+#define N_orderhigh 5
+
+#define T_paymentlow 1
+#define T_paymenthigh 2
+
+#define C_pizza 10
+
+#define P_fail 5
+
+#define T_prep 1
+#define T_bake 10
+#define T_pack 2
+
+#define T_dellow 5
+#define T_delhigh 15
+
+typedef struct ResourceInfo
+{
+    unsigned int size;
+    unsigned int _MaxSize;
+
+    pthread_mutex_t muxtex;
+    pthread_cond_t cond;
+} ResourceInfo;
+
+typedef struct Args
+{
+    ResourceInfo* Resources;
+    int id;
+} Args;
+
+
+#define bool unsigned char
+#define TRUE 1
+#define FALSE 0
 
